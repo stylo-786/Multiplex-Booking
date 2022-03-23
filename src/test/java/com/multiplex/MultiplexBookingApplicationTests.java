7824dce0a7169c2acb7f3db99a4d461bb25ba885
stylo-786 <styloshadab786@gmail.com>
2022-03-23 16:29:27 +0530

    first commit

---
+++ src/test/java/com/multiplex/MultiplexBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplexBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
