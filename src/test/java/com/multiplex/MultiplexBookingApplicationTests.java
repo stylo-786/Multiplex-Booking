7c5038647d27be8dd623d35c00ba791db1e769d9
stylo-786 <styloshadab786@gmail.com>
2022-03-25 14:08:26 +0530

    adding test imp

--- src/test/java/com/multiplex/MultiplexBookingApplicationTests.java
+++ src/test/java/com/multiplex/MultiplexBookingApplicationTests.java
@@ -1,13 +1,40 @@
 package com.multiplex;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.multiplex.dao.MoviesRepository;
+import com.multiplex.dao.ShowsRepository;
+import com.multiplex.dao.UsersRepository;
+import com.multiplex.entity.Movies;
+
 @SpringBootTest
-class MultiplexBookingApplicationTests {
+class TicketBookingApplicationTests {
+	@Autowired
+	MoviesRepository movierepo;
+	@Autowired
+	UsersRepository userrepo;
+	@Autowired
+	ShowsRepository showsrepositary;
+	@Test
+	void testMovies() {
+		Movies movie = new Movies();
+		movie.setMovieId(6);
+		movie.setMovieName("rrr");
+		movierepo.save(movie);
+		assertNotNull(movierepo.findById(6).get());
+	}
 
 	@Test
-	void contextLoads() {
+	void testLogin() {
+		assertNotNull(userrepo.findByEmailIdAndPassward("shabbir29@gmail.com", "123456789"));
+	}
+	@Test
+	void testGetShow(){
+		assertNotNull(showsrepositary.findById(1));
 	}
 
 }
